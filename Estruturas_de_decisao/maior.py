236028918c66b00f06adc448650ec5468693a4cf
Fabio-Coder <0xfab1015@gmail.com>
2021-04-12 10:32:10 -0300

    impleentado modulo maior.py 1- Faça um Programa que peça dois números e imprima o maior deles.

---
+++ Estruturas_de_decisao/maior.py
@@ -0,0 +1,15 @@
+def maior(n1: int, n2: int):
+    """
+    1- Faça um Programa que peça dois números e imprima o maior deles.
+
+    >>> maior (1,2)
+    2
+
+    >>> maior (20, 13)
+    20
+    
+    :param n1: Prmeiro valor : inteiro
+    :param n2: Segundo valor : inteiro
+    :return:
+    """
+    return (n1 if n1 > n2 else n2)
