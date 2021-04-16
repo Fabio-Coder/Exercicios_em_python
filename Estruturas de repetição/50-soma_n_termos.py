94313adb00c8847076da9517442ace36c40a9e94
Fabio-Coder <0xfab1015@gmail.com>
2021-04-16 13:10:52 -0300

    Impementado o modulo soma-n_termos.py 50 - Sendo H= 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, Faça um programa que calcule o valor de H com N termos.

---
+++ Estruturas de repetição/50-soma_n_termos.py
@@ -0,0 +1,21 @@
+def soma_n_termos(n):
+    """
+    50 - Sendo H= 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, Faça um programa que calcule o valor de H com N termos.
+
+    >>> soma_n_termos(1)
+    1.0
+    >>> soma_n_termos(10)
+    2.93
+    >>> soma_n_termos(1000)
+    7.49
+
+    :param n:
+    :return:
+    """
+
+    soma = 0
+
+    for i in range(1,n+1):
+        soma += 1 / i
+
+    print(round(soma,2))
