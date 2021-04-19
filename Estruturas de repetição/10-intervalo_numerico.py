e348ce1c7a385185da51201cb5b5fa9ed4f81ef3
Fabio Coder <0xfab1015@gmail.com>
2021-04-18 21:24:20 -0300

    Implementado modulo 10-intervalo_numerico.py receba dois numeros inteiros e gere os inteiros no intervalo entre eles.

---
+++ Estruturas de repetição/10-intervalo_numerico.py
@@ -0,0 +1,17 @@
+def intervalo_numerico(n1,n2):
+    """
+    10 - Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido
+    por eles.
+
+    >>> intervalo_numerico(1,5)
+    2
+    3
+    4
+
+    :param n1:
+    :param n2: 
+    :return:
+    """
+
+    for i in range(n1+1,n2):
+        print(i)
