e66438f4a9bfd48964bf0b0f6fba805c302feee5
Fabio-Coder <0xfab1015@gmail.com>
2021-04-19 16:47:56 -0300

    Impementado o modulo ips_validos.py Em processo. Problema na lina 28 ao tentar ler cada linha do arquivo em um item da lista ip_sem_formatar

--- Exercicios_com_arquivos/ips_validos.py
+++ Exercicios_com_arquivos/ips_validos.py
@@ -4,22 +4,56 @@ def ip_valido():
     relatório dos endereços IP válidos e inválidos.
 
 
+
     :param arquivo:
     :return:
     """
 
     arquivo = open("ip.txt", "r")
-    saida = open("lista_ips.txt","x")
-    ip_sem_formatar = ''
-
-   for linha in arquivo:
-       ip_sem_formatar = 
-
-    print(arquivo.read())
 
 
+    ip_sem_formatar = []
+    ips_validos = []
+    ips_nao_validos = []
+    i = 0
+    for linha in arquivo:
+        ip_sem_formatar[i] = linha
+        i += 1
+
+    for i in ip_sem_formatar:
+        if checar_ip(ip_sem_formatar[i]):
+            ips_validos[i] = ip_sem_formatar[i]
+        else:
+            ips_nao_validos[i] = ip_sem_formatar[i]
+
+    saida = open("lista_ips.txt", "w")
+    saida.write('[Endereços válidos:]')
+
+    for i in ips_validos[i]:
+        saida = open("lista_ips.txt", "a")
+        saida.write(ips_validos[i])
+
+    saida = open("lista_ips.txt", "a")
+    saida.write('[Endereços inválidos:]')
+
+    for i in ips_nao_validos[i]:
+        saida = open("lista_ips.txt", "a")
+        saida.write(ips_nao_validos[i])
+
+    saida.close()
+
+def checar_ip(ip):
+    quartos = ip.split('.')
+    ok = 0
+    for i in quartos:
+        if not quartos[i].isnumeric() or quartos[i] > 255:
+            return False
+        else:
+            ok += 1
+
+    return True
 
 
 if __name__ == '__main__':
-    ip_valido()
+    ip_valido
 
