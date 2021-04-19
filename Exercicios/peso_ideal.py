0f162203462a42b99a35ea49372b29c02e863f2b
Fabio Coder <0xfab1015@gmail.com>
2021-04-18 21:12:58 -0300

    Implementado modulo peso_ideal.py Crie um programa que calcule o peso ideal de uma pessoa

---
+++ Exercicios/peso_ideal.py
@@ -0,0 +1,12 @@
+def peso_ideal(altura):
+    """
+    12- Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+        usando a seguinte fórmula: (72.7*altura) - 58
+    >>> peso_ideal(1.71)
+    66.32
+
+    :param altura:
+    :return:
+    """
+
+    print(round(((72.7 * altura) - 58),2))
