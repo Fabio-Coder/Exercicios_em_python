17b55ddd182858cb0729c0f5aba653702bc1856c
Fabio-Coder <0xfab1015@gmail.com>
2021-04-25 17:55:09 -0300

    melhorias no codigo do quadrado mágico

--- Exercicios_com_funcoes/14_quadrado_magico.py
+++ Exercicios_com_funcoes/14_quadrado_magico.py
@@ -12,31 +12,21 @@ def quadrado_magico():
     Dica: produza todas as combinações possíveis e verifique a soma quando completar cada quadrado.
     Usar um vetor de 1 a 9 parece ser mais simples que usar uma matriz 3x3.
 
-    >>> quadrado_magico()
-    ok
-
     :return:
     """
-
-    quadrado = [8, 3, 4, 1, 5, 9, 6, 7, 2]
 
     if verifica_quadrado_magico(quadrado):
         print('ok')
 
-
 def verifica_quadrado_magico(vetor):
     """
-    0   1   2
-    3   4   5
-    6   7   8
-
-    :param vetor:
-    :return:
+    :param vetor: lista com os valores do quadrado magico
+    :return: True se o quadrado for mágico
     """
     soma = []
-    soma.append(vetor[0] + vetor[1] + vetor[2])
-    soma.append(vetor[3] + vetor[4] + vetor[5])
-    soma.append(vetor[6] + vetor[7] + vetor[8])
+    soma.append(sum(vetor[0:2]))
+    soma.append(sum(vetor[3:5]))
+    soma.append(sum(vetor[6:8]))
     soma.append(vetor[0] + vetor[3] + vetor[6])
     soma.append(vetor[1] + vetor[4] + vetor[7])
     soma.append(vetor[2] + vetor[5] + vetor[8])
@@ -47,3 +37,9 @@ def verifica_quadrado_magico(vetor):
         return True
 
     return False
+
+if __name__ == '__main__':
+
+    quadrado = [8, 3, 4, 1, 5, 9, 6, 7, 2]
+
+    quadrado_magico(quadrado)
